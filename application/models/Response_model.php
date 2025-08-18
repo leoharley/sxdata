e1f07ffcc2a6f4d6e105912a44fcb2046f296feb
Leonardo Harley <leoharleygoncalves@gmail.com>
2025-08-18 19:53:59 -0300

    ...

--- application/models/Response_model.php
+++ application/models/Response_model.php
@@ -1399,7 +1399,7 @@ private function analyze_option_responses($question_id, $filters, $total_respons
             // Para radio, buscar em selected_options (JSON contém o ID da opção)
             $this->db->select('COUNT(*) as count');
             $this->db->from('question_responses qr');
-            //$this->db->join('form_responses fr', 'qr.form_response_id = fr.id', 'inner');
+            $this->db->join('form_responses fr', 'qr.form_response_id = fr.id', 'inner');
             $this->db->where('qr.question_id', $question_id);
             
             // Buscar no JSON - verificar se o ID da opção está no array JSON
