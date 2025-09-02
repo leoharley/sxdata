b63b2e5571c06cfecc1b299a7e2c1c3087fbdb65
Leonardo Harley <leoharleygoncalves@gmail.com>
2025-09-02 09:22:29 -0300

    ....

--- application/views/admin/users/index.php
+++ application/views/admin/users/index.php
@@ -92,7 +92,7 @@
                             <span class="badge bg-<?= $color ?>"><?= ucfirst($user->role) ?></span>
                         </td>
                         <td>
-                            <span class="badge <?= $user->is_active == 0 ? 'bg-danger' : 'bg-success'?>"><?= $user->is_active == 0  ? 'Desativar' : 'Ativar' ?></span>
+                            <span class="badge <?= $user->is_active == false ? 'bg-danger' : 'bg-success'?>"><?= $user->is_active == false  ? 'Desativar' : 'Ativar' ?></span>
                         </td>
                         <td>
                             <?php if (isset($user->updated_at) && $user->updated_at): ?>
