4eb65b121f1343fd66c29a00ccaceb8d7e2cf704
Leonardo Harley <leoharleygoncalves@gmail.com>
2025-09-07 20:20:59 -0300

    ....

--- application/controllers/Responses.php
+++ application/controllers/Responses.php
@@ -176,11 +176,11 @@ class Responses extends CI_Controller {
             ); */
             
             // Aumentar limites para exportação
-         /*   ini_set('memory_limit', '2024M');
-            ini_set('max_execution_time', 600); */
+            ini_set('memory_limit', '2024M');
+            ini_set('max_execution_time', 600);
             
             // Gerar arquivo Excel com dados brutos
-            $this->_generate_raw_data_excel($filters, $questionnaire_id);
+        //    $this->_generate_raw_data_excel($filters, $questionnaire_id);
             
             // Log sucesso da exportação
         /*    $this->Response_model->log_export_activity(
