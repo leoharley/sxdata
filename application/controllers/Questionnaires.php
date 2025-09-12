a0e6a7b6a465c5e5fdc6df0d160509c8484e4b6a
Leonardo Harley <leoharleygoncalves@gmail.com>
2025-09-12 00:08:24 -0300

    ....

--- application/controllers/Questionnaires.php
+++ application/controllers/Questionnaires.php
@@ -281,8 +281,6 @@ class Questionnaires extends CI_Controller {
                                 'conditional_logic' => $question['conditional_logic']
                             );
 
-                            var_dump($question['conditional_logic']);exit;
-                            
                             $question_id = null;
 
                             // Verificar se é pergunta existente ou nova
