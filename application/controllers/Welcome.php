da47eba59dbcf4ce7b66ebef96b706ed1debab1e
Leonardo Harley <leoharleygoncalves@gmail.com>
2026-03-03 21:15:58 -0300

    .....

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -24,7 +24,11 @@ class Welcome extends CI_Controller {
 	}
 
 	public function politica_de_privacidade() {
-        exit;
+        $data['title'] = 'Política de Privacidade - SXData';
+
+        $this->load->view('admin/header', $data);
+        $this->load->view('admin/privacy', $data);
+        $this->load->view('admin/footer');
     }
-	
+
 }
