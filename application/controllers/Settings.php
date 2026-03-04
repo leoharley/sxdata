d8088644a6f1ee43e313157c6b18ea46af58adae
Leonardo Harley <leoharleygoncalves@gmail.com>
2026-03-03 21:11:46 -0300

    ....

--- application/controllers/Settings.php
+++ application/controllers/Settings.php
@@ -20,10 +20,6 @@ class Settings extends CI_Controller {
         $this->load->view('admin/footer');
     }
 
-    public function politica_de_privacidade() {
-        exit;
-    }
-
     public function update() {
         $settings = $this->input->post('settings');
         
